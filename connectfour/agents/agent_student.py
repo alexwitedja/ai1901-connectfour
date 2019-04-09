7cf60a300fdd294fcdce1cca3534336a9502dc19
AleXW <alexwitedja@hotmail.co.id>
2019-04-09 14:41:34 +1000

    changes to heuristic calculation

--- connectfour/agents/agent_student.py
+++ connectfour/agents/agent_student.py
@@ -1,11 +1,12 @@
 from connectfour.agents.computer_player import RandomAgent
 import random
 
+# need to implement alpha beta pruning
+
 class StudentAgent(RandomAgent):
     def __init__(self, name):
         super().__init__(name)
         self.MaxDepth = 1
-
 
     def get_move(self, board):
         """
@@ -24,11 +25,13 @@ class StudentAgent(RandomAgent):
             moves.append( move )
             vals.append( self.dfMiniMax(next_state, 1) )
 
+        print(vals)
         bestMove = moves[vals.index( max(vals) )]
         return bestMove
 
     def dfMiniMax(self, board, depth):
         # Goal return column with maximized scores of all possible next states
+        # Number of possible states is the number of columns
         
         if depth == self.MaxDepth:
             return self.evaluateBoardState(board)
@@ -49,8 +52,38 @@ class StudentAgent(RandomAgent):
             bestVal = max(vals)
 
         return bestVal
+    
+    def alphabeta(self, board, depth, alpha, beta, maximizingPlayer=1):
+        
+        #inital call callbeta(origin, depth, -inf, +inf, 1)
+
+        valid_moves = board.valid_moves()
+
+        if depth == 0:
+            return self.evaluateBoardState(board)
+        if maximizingPlayer == 1:
+            value = alpha
+            for move in valid_moves:
+                next_state = board.next_state(self.id, move[1])
+                value = max([value, self.alphabeta(next_state, depth - 1, alpha, beta, 1)])
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return value
+        else:
+            value = beta
+            for move in valid_moves:
+                next_state = board.next_state(self.id, move[1])
+                value = min([value, self.alphabeta(next_state, depth - 1, alpha, beta, 2)])
+                beta = min(beta,value)
+                if alpha >= beta:
+                    break
+            return value
 
     def evaluateBoardState(self, board):
+        # Heuristics ideas, check for fours, threes and twos.
+        # Threes worth more than two.
+        # center columns have more alignments. explore center first.
         """
         Your evaluation function should look at the current state and return a score for it. 
         As an example, the random agent provided works as follows:
@@ -80,29 +113,43 @@ class StudentAgent(RandomAgent):
             next_state(turn)
             winner()
         """
+        score = self.scoreBoard(board, 1)
+
+        if score == 0:
+            score = random.randint(1,9)
+
+        return score
+    
+    def scoreBoard(self, board, playerno=1):
+
         score = 0
-        # Checking for guaranteed win states.
-        
-        if(self.checkFours(board, 1)):
-            return 100
-        elif(self.checkFours(board,2)):
-            return -100
+        count = 0
+        # Horizontal
+        for row in range(board.height):
+            for col in range(board.width):
+                if board.get_cell_value(row, col) == playerno:
+                    count += 1
+                    if count == 4:
+                        score += 100
+                    elif count == 3 and board.valid_move(row, col+1):
+                        score += 10
+                else:
+                    count = 0
 
-		# Checks twos threes and fours		
-        twos = self.checkTwos(board, 1)
-        enemyTwos = self.checkTwos(board, 2)
-        threes = self.checkThrees(board, 1) * 2
-        enemyThrees = self.checkThrees(board, 2) * 2
+        # Vertical
+        for col in range(board.width):
+            for row in range(board.height):
+                if board.get_cell_value(row, col) == playerno:
+                    count += 1
+                    if count == 4:
+                        score += 100
+                    elif count == 3 and board.valid_move(row-3, col):
+                        score += 10
+                else:
+                    count = 0
 
-        score = twos + threes - enemyTwos - enemyThrees
+        # Diagonals
+        # Forward diagonal
 
         return score
 
-    def checkFours(self, board, playerNo=1):
-        return 0
-
-    def checkThrees(self, board,  playerNo=1): 
-        return 0
-
-    def checkTwos(self, board,  playerNo=1):
-        return 0
